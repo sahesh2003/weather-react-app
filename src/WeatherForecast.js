f034b663c34843c13d112e16e7392d6c994b4bd0
sahesh2003 <128036977+sahesh2003@users.noreply.github.com>
2023-10-05 13:49:12 +0330

    remove fahrenheit

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -29,7 +29,7 @@ export default function WeatherForecast(props) {
             } else {
               return null;
             }
-          })}
+          })};
         </div>
       </div>
     );
--- src/WeatherTempreture.js
+++ src/WeatherTempreture.js
@@ -2,39 +2,39 @@ import React, { useState } from "react";
 
 export default function WeatherTempreture(props) {
   let [unit, setUnit] = useState("celsius");
-  function showFahrenheit(event) {
-    event.preventDefault();
-    setUnit(`fahrenheit`);
-  }
-  function showCelsius(event) {
-    event.preventDefault();
-    setUnit(`celsius`);
-  }
+  // function showFahrenheit(event) {
+  //   event.preventDefault();
+  //   setUnit(`fahrenheit`);
+  // }
+  // function showCelsius(event) {
+  //   event.preventDefault();
+  //   setUnit(`celsius`);
+  // }
 
   if (unit === "celsius") {
     return (
       <div>
         <span className="temperature">{props.celsius}</span>
         <span className="units">
-          °C |{" "}
-          <a href="/" onClick={showFahrenheit}>
+           °C
+          {/* <a href="/" onClick={showFahrenheit}>
             °F
-          </a>
+          </a> */}
         </span>
       </div>
     );
   } else {
-    let fahrenheit = Math.round((props.celsius * 9) / 5 + 32);
-    return (
-      <div>
-        <span className="temperature">{fahrenheit}</span>
-        <span className="units">
-          <a href="/" onClick={showCelsius}>
-            °C 
-          </a>
-          {" "} | °F
-        </span>
-      </div>
-    );
+    // let fahrenheit = Math.round((props.celsius * 9) / 5 + 32);
+    // return (
+    //   <div>
+    //     <span className="temperature">{fahrenheit}</span>
+    //     <span className="units">
+    //       <a href="/" onClick={showCelsius}>
+    //         °C 
+    //       </a>
+    //       {" "} | °F
+    //     </span>
+    //   </div>
+    // );
   }
 }
